1d18ca01b441fde9817f97bcb40aafdcf7750afe
tbamforth <thomas_bamforth@kenan-flagler.unc.edu>
2025-02-25 15:00:41 -0500

    Updated formatting
    Change all assets

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,7 +3,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `UNC Basketball AI Bot`;
 export const MESSAGE_PLACEHOLDER: string = `TAR`;
 export const FOOTER_MESSAGE: string = `GO HEELS!`;
-export const CLEAR_BUTTON_TEXT: string = `CLEAR`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR BUTTON`;
 export const PAGE_TITLE: string = `UNC Basketball`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
