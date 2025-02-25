34cddeabcaf79dbf850e9ed73f2792e94004c766
tbamforth <thomas_bamforth@kenan-flagler.unc.edu>
2025-02-25 13:44:44 -0500

    Update ui.ts
    UNC Basketball AI

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
-import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
+import { UNC Basketball AI Bot, Thomas Bamforth } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Thomas's AI Bot`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Thanks for coming!`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CHAT_HEADER: string = `UNC Basketball AI Bot`;
+export const MESSAGE_PLACEHOLDER: string = `TAR`;
+export const FOOTER_MESSAGE: string = `GO HEELS!`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR BUTTON`;
+export const PAGE_TITLE: string = `UNC Basketball`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
