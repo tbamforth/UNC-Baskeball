96ca29224676c09818995cb525d48b79e3cc4705
tbamforth <thomas_bamforth@kenan-flagler.unc.edu>
2025-02-25 13:50:19 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,4 +1,4 @@
-import { 'UNC Basketball AI Bot', 'Thomas Bamforth' } from "@/configuration/identity";
+import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `UNC Basketball AI Bot`;
 export const MESSAGE_PLACEHOLDER: string = `TAR`;
